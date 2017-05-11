1b305046c8733fa3bc4fb976853b9197084daa27
aolite <aitorcorchero@gmail.com>
2017-05-11 22:52:44 +0200

    Incorporation of the service

--- src/app/hello.ts
+++ src/app/hello.ts
@@ -1,41 +1,40 @@
 import {Component} from '@angular/core';
 
+import { HeroService } from './hero.service';
+
 export class Hero {
   id: number;
   name: string;
 }
 
-var HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-];
-
 @Component({
   selector: 'fountain-app',
   styleUrls: ['app/hello.css'],
-  template: require('./hello.html')
+  template: require('./hello.html'),
+  providers: [HeroService]
 })
 export class HelloComponent {
 
   public title: string;
   public selectedHero: Hero;
-  public heroes = HEROES;
+  public heroes: Hero[];
 
 
-  constructor() {
+  constructor(private heroService: HeroService) {
     this.title = 'Tour of heroes';
   }
 
   onSelect (hero: Hero): void {
     this.selectedHero = hero;
   }
+
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
 }
 
