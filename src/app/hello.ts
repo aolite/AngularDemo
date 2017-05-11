3f3c1fc75c6b3663a26893383cb151133510ec3f
aolite <aitorcorchero@gmail.com>
2017-05-11 18:19:27 +0200

    Definition of the css file

--- src/app/hello.ts
+++ src/app/hello.ts
@@ -1,23 +1,36 @@
 import {Component} from '@angular/core';
 
+export class Hero {
+  id: number;
+  name: string;
+}
+
+var HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
+
 @Component({
   selector: 'fountain-app',
+  styleUrls: ['app/hello.css'],
   template: require('./hello.html')
 })
 export class HelloComponent {
+
   public title: string;
 
-  public hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  public heroes = HEROES;
 
   constructor() {
     this.title = 'Tour of heroes';
   }
 }
 
-export class Hero {
-  id: number;
-  name: string;
-}
