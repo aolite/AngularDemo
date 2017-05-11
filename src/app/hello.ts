0520a50f804fb6c679ae42fb9136cbf88498f26e
aolite <aitorcorchero@gmail.com>
2017-05-11 23:38:24 +0200

    Add Promises to the service

--- src/app/hello.ts
+++ src/app/hello.ts
@@ -29,7 +29,7 @@ export class HelloComponent {
   }
 
   getHeroes(): void {
-    this.heroes = this.heroService.getHeroes();
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
   }
 
   ngOnInit(): void {
